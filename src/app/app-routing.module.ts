99e08c8fd01b2b80352fcb07448646674fb140e5
walidtalal <alwaleedhawas@gmail.com>
2023-09-26 17:28:54 +0200

    finish 26-9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { SuccessComponent } from './success/success.component';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
+  // { path: '', redirectTo: '/registration', pathMatch: 'full' },
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
 
+  { path: 'registration', component: SignupComponent },
+  { path: 'success', component: SuccessComponent },
+  { path: 'login', component: LoginComponent },
+  // { path: 'profile', component: ProfileComponent },
+  { path: 'profile/:username/:id', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  user: any = {
+    email: '',
+    password: '',
+  };
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onSubmit() {
+    if (this.authService.login(this.user.email, this.user.password)) {
+      // Retrieve user data from localStorage
+      const userData = localStorage.getItem('users');
+
+      if (userData) {
+        // Parse the JSON string back to an array of user objects
+        const users = JSON.parse(userData);
+
+        // Find the user with the matching email (assuming each user has a unique email)
+        const loggedInUser = users.find(
+          (user: any) => user.email === this.user.email
+        );
+
+        if (loggedInUser) {
+          // Redirect to the profile page with the actual username and id
+          this.router.navigate([
+            '/profile',
+            loggedInUser.username,
+            loggedInUser.id,
+          ]);
+        } else {
+          //  the case where the user is not found
+          alert('User not found.');
+        }
+      } else {
+        // the case where user data is not found in localStorage
+        alert('User data not found in localStorage.');
+      }
+    } else {
+      //  an error message
+      alert('Invalid email or password. Please try again.');
+    }
+  }
 }
